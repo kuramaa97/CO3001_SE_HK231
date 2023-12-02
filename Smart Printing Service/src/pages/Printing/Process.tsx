c3810c5ebf7c8ab4bed8b10977fe400ca8abec15
nyankirie <nyankirie@gmail.com>
2023-12-03 02:22:28 +0700

    modify upload, process, setting, add sum weird shjt, downgrade menulist (lel)

--- Smart Printing Service/src/pages/Printing/Process.tsx
+++ Smart Printing Service/src/pages/Printing/Process.tsx
@@ -4,24 +4,22 @@ import LinearProgress from '@mui/material/LinearProgress';
 import CircularProgress from '@mui/material/CircularProgress';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Process: React.FC = () => {
   const [stage, setStage] = useState(0);
-  const nav = useNavigate();
 
   useEffect(() => {
     const timer = setInterval(() => {
       setStage((prevStage) => {
-        if (prevStage <= 2) {
+        if (prevStage <= 1) {
           return prevStage + 1;
         } else {
           clearInterval(timer);
           return prevStage;
         }
       });
-    }, 4000); // 5 seconds
-
+    }, 2000); // 5 seconds
     return () => clearInterval(timer); // Clean up on component unmount
   }, []);
 
@@ -43,14 +41,14 @@ const Process: React.FC = () => {
           <CircularProgress className="mb-24 mt-24 w-96 h-96" size={100} />
           <Typography variant="body1" sx={{ fontSize: '1.5rem', mt: 10, color: 'black' }}>Tài liệu của bạn đang được in</Typography>
           </>
-      ) : stage === 2 ?(
-        <div className='flex flex-col items-center justify-start cursor-default  bg-[#F2F0F0] h-screen w-screen'>
-          <h1 className="text-blue-500 font-bold text-5xl mt-10 mb-24">Thành công</h1>
-          <CheckCircleIcon style={{ fontSize: 200, color: 'black' }} className='mb-10s'/>
-          <Typography variant="body1" sx={{ fontSize: '2rem', mt: 10, color: 'black' }}>Đã in thành công</Typography>
-        </div>
       ) : (
-        <>{nav('/')}</>
+        <Link to={"/"}>
+          <div className='flex flex-col align-middle items-center cursor-default bg-[#F2F0F0] h-screen w-screen'>
+            <h1 className="text-blue-500 font-bold text-5xl mt-10 mb-24">Thành công</h1>
+            <CheckCircleIcon style={{ fontSize: 200, color: 'black' }} className='mb-10s'/>
+            <span className='text-xl mt-10'>Nhấn vào màn hình để quay về trang chủ</span>
+          </div>
+        </Link>
       )}
     </div>
   );
